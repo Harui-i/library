61e540b6e3cd88196dbf3179999e65503223cb33
Haruki Oshiro <23120005@ed.tmu.ac.jp>
2024-05-15 21:56:16 +0900

    verifyURLを追加するなど

--- unabstracted_libs/segment-tree/aoj-dsl-2-d(RUQ).cpp
+++ unabstracted_libs/segment-tree/aoj-dsl-2-d(RUQ).cpp
@@ -1,14 +1,6 @@
-// https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=DSL_2_D
-#include <bits/stdc++.h>
-using namespace std;
-typedef long long ll;
-typedef unsigned int uint;
-template<class T> inline bool chmax(T& a, const T& b) { if (a < b) { a = b; return true; } return false; }
-template<class T> inline bool chmin(T& a, const T& b) { if (b < a) { a = b; return true; } return false; }
-const int INTINF = 1000001000;
-const int INTMAX = 2147483647;
-const ll LLMAX = 9223372036854775807;
-const ll LLINF = 1000000000000000000;
+#define PROBLEM https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=DSL_2_D
+
+#include <../template/template.hpp>
 
 vector<pair<int,int>>dat;
 int sz;
