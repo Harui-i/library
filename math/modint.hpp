5ff91c27261e040db45170b0acc7cf2763ab6f91
Haruki Oshiro <23120005@ed.tmu.ac.jp>
2024-05-17 18:23:08 +0900

    modintを追加

---
+++ math/modint.hpp
@@ -0,0 +1,79 @@
+template<int MOD>
+struct static_modint {
+    int value;
+
+    constexpr static_modint() : value(0) {}
+
+    constexpr static_modint(long long v) {
+        value = int(((v % MOD) + MOD) % MOD);
+    }
+
+    constexpr static_modint& operator+=(const static_modint& other) {
+        if ((value += other.value) >= MOD) value -= MOD;
+        return *this;
+    }
+
+    constexpr static_modint& operator-=(const static_modint& other) {
+        if ((value -= other.value) < 0) value += MOD;
+        return *this;
+    }
+
+    constexpr static_modint& operator*=(const static_modint& other) {
+        value = int((long long)value * other.value % MOD);
+        return *this;
+    }
+
+    constexpr static_modint operator+(const static_modint& other) const {
+        return static_modint(*this) += other;
+    }
+
+    constexpr static_modint operator-(const static_modint& other) const {
+        return static_modint(*this) -= other;
+    }
+
+    constexpr static_modint operator*(const static_modint& other) const {
+        return static_modint(*this) *= other;
+    }
+
+    constexpr static_modint pow(long long exp) const {
+        static_modint base = *this, res = 1;
+        while (exp > 0) {
+            if (exp & 1) res *= base;
+            base *= base;
+            exp >>= 1;
+        }
+        return res;
+    }
+
+    constexpr static_modint inv() const {
+        return pow(MOD - 2);
+    }
+
+    constexpr static_modint& operator/=(const static_modint& other) {
+        return *this *= other.inv();
+    }
+
+    constexpr static_modint operator/(const static_modint& other) const {
+        return static_modint(*this) /= other;
+    }
+
+    int val() const {
+      return this.val;
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, const static_modint& mi) {
+        return os << mi.value;
+    }
+
+    friend std::istream& operator>>(std::istream& is, static_modint& mi) {
+        long long x;
+        is >> x;
+        mi = static_modint(x);
+        return is;
+    }
+};
+
+template <int mod>
+using modint = static_modint<mod>;
+using modint998244353 = modint<998244353>;
+using modint100000007 = modint<100000007>;
