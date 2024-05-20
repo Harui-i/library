a44e39832d70aab16a71614c500490c15aa2b8d2
Haruki Oshiro <23120005@ed.tmu.ac.jp>
2024-05-21 00:13:35 +0900

    add input kousokuka

--- test/verify/yosupo-inv-of-formal-power-series.test.cpp
+++ test/verify/yosupo-inv-of-formal-power-series.test.cpp
@@ -8,6 +8,7 @@ using mint = modint998244353;
 
 
 int main() {
+  ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
   int N; cin >> N;
   vector<modint998244353>a(N);
   for(int i=0; i<N; i++) cin >> a[i];
