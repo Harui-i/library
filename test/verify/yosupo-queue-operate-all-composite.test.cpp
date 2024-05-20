d92d5f53a24b058fe3d1844e5f59fac5828dfd7c
Haruki Oshiro <23120005@ed.tmu.ac.jp>
2024-05-21 00:35:00 +0900

    SWAG(foldable deque)

---
+++ test/verify/yosupo-queue-operate-all-composite.test.cpp
@@ -0,0 +1,40 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/queue_operate_all_composite"
+
+#include "../../template/template.hpp"
+#include "../../math/modint.hpp"
+#include "../../structure/slide-window-aggregation.hpp"
+
+using mint = modint998244353;
+using mpp = pair<mint,mint>;
+
+mpp op(mpp lf, mpp rf) {
+  return {rf.first * lf.first, rf.first * lf.second + rf.second};
+}
+
+mpp e() {
+  return {1, 0};
+}
+
+int main() {
+  ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+  FoldableDeque<mpp,op,e> fdq;
+
+  int Q; cin >> Q;
+  for(int i=0; i<Q; i++) {
+    int op; cin >> op;
+    if (op == 0) {
+      int a,b; cin >> a >> b;
+      fdq.push_back({mint(a), mint(b)});
+    }
+
+    else if (op == 1) {
+      fdq.pop_front();
+    }
+    else if (op == 2) {
+      int x; cin >> x;
+      mpp all_prod = fdq.all_prod();
+      cout << mint(x) * all_prod.first + all_prod.second << endl;;
+    }
+  }
+  return 0;
+}
