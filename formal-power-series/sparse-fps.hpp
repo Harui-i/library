bde79b3ddac810aa286c626f0d69a255d5fb8f4b
Haruki Oshiro <23120005@ed.tmu.ac.jp>
2024-05-31 23:23:37 +0900

    sparse inv

---
+++ formal-power-series/sparse-fps.hpp
@@ -0,0 +1,65 @@
+#pragma once
+#include <vector>
+using namespace std;
+
+#include "formal-power-series.hpp"
+
+// calculate inverse of f(sparse)
+// deg : -1 + ( maximum degree of g )
+template <typename mint>
+FPS inv_sparse(const vector<pair<int,mint>>& f, int deg) {
+  assert(deg >= 0);
+  for(int i=0; i<(int)f.size()-1; i++) assert(f[i].first < f[i+1].first); 
+  assert(f[0].first == 0 && f[0].second != mint(0));
+
+  mint f0inv = f[0].second.inv();
+  vector<mint> g(deg); g[0] = f0inv;
+  for(int i=0; i<deg-1; i++) {
+    for (pair<int,mint> pim : f) {
+      if (i+1 - pim.first >= 0) g[i+1] -= pim.second * g[i+1 - pim.first];
+      else continue;
+    }
+    g[i+1] *= f0inv;
+  }
+
+  return g;
+}
+
+template <typename mint>
+FPS inv_sparse(const FPS& f, int deg) {
+  vector<pair<int,mint>> vpim;
+  for(int i=0; i<f.size(); i++) if (f[i] != mint(0)) vpim.emplace_back(i, f[i]);
+
+  return inv_sparse(vpim, deg);
+}
+
+
+/* tabun baggute masu. TODO
+template<typename mint>
+FPS multiply_sparse(const FPS& f, const vector<pair<int,mint>>& g, int deg) {
+
+  
+
+  FPS ret(deg);
+  for (pair<int,mint> pim : g) {
+    assert(pim.second != 0);
+    if (pim.second == 0) continue;
+
+    for(int i=0; i<f.size(); i++) {
+      if (i+pim.first >= ret.size()) continue;
+      if (f[i] != mint(0) && pim.second != mint(0)) ret[i+pim.first] += pim.second * f[i];
+    }
+  }
+
+  return ret;
+}
+
+template <typename mint>
+FPS multiply_sparse(const FPS& f, const FPS& g) {
+  vector<pair<mint,int>> vpmi;
+
+  for(int i=0; i<g.size(); i++) if (g[i] != mint(0)) vpmi.emplace_back(i, g[i]);
+
+  return multiply_sparse(f, vpmi);
+}
+*/
---
+++ test/verify/yosupo-inv-of-formal-power-series-sparse.test.cpp
@@ -0,0 +1,20 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/inv_of_formal_power_series_sparse"
+
+#include "../../template/template.hpp"
+#include "../../math/modint.hpp"
+#include "../../formal-power-series/formal-power-series.hpp"
+#include "../../formal-power-series/sparse-fps.hpp"
+
+using mint = modint998244353;
+
+
+int main() {
+  ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+  int N, K; cin >> N >> K;
+  vector<pair<int,mint>> ia(K);
+  for(int i=0; i<K; i++) cin >> ia[i].first >> ia[i].second;
+
+  FPS inv = inv_sparse(ia, N);
+  for(int i=0; i<N; i++) cout << inv[i] << " ";
+  cout << endl;
+}
