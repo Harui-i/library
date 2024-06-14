8d938952ae8cc3a7fe103c1205655d0b1c0adfe8
Haruki Oshiro <23120005@ed.tmu.ac.jp>
2024-06-14 18:26:48 +0900

    さすがにnamespaceの使い方が頭おかしかったので修正

--- graph/dijkstra.hpp
+++ graph/dijkstra.hpp
@@ -2,61 +2,59 @@
 
 #include "graph/graph_template.hpp"
 
-namespace std {
-  template <typename T>
-  vector<T> dijkstra(const Graph<T>& graph, int start) {
-    int N = (int)graph.size();
-    vector<T>dist(N, T(-1));
-    using P = pair<T, int>;
+template <typename T>
+std::vector<T> dijkstra(const Graph<T>& graph, int start) {
+  int N = (int)graph.size();
+  vector<T>dist(N, T(-1));
+  using P = pair<T, int>;
 
-    priority_queue<P, vector<P>, greater<P>>que;
+  priority_queue<P, vector<P>, greater<P>>que;
 
-    que.push(make_pair(T(0), start));
-    dist[start] = T(0);
+  que.push(make_pair(T(0), start));
+  dist[start] = T(0);
 
-    while (!que.empty()) {
-      P front = que.top(); que.pop();
+  while (!que.empty()) {
+    P front = que.top(); que.pop();
 
-      if (dist[front.second] < front.first) continue;
+    if (dist[front.second] < front.first) continue;
 
-      for (Edge ed : graph[front.second]) {
+    for (Edge ed : graph[front.second]) {
 
-        if (dist[ed.to] == T(-1) || dist[ed.to] > front.first + ed.cost) {
-          dist[ed.to] = front.first + ed.cost;
-          que.emplace(dist[ed.to], ed.to);
-        }
+      if (dist[ed.to] == T(-1) || dist[ed.to] > front.first + ed.cost) {
+        dist[ed.to] = front.first + ed.cost;
+        que.emplace(dist[ed.to], ed.to);
       }
     }
-
-    return dist;
   }
-  
-  template <typename T>
-  pair<vector<T>, vector<int>> dijkstra_path(const Graph<T>& graph, int start) {
-    int N = (int)graph.size();
 
-    using P = pair<T, int>;
-    vector<T>dist(N, T(-1));
-    vector<int>prev(N, -1);
+  return dist;
+}
 
-    priority_queue<P, vector<P>, greater<P>>que;
-    que.push(make_pair(T(0), start));
-    dist[start] = T(0);
+template <typename T>
+pair<vector<T>, vector<int>> dijkstra_path(const Graph<T>& graph, int start) {
+  int N = (int)graph.size();
 
-    while (!que.empty()) {
-      P front = que.top(); que.pop();
+  using P = pair<T, int>;
+  vector<T>dist(N, T(-1));
+  vector<int>prev(N, -1);
 
-      if (dist[front.second] < front.first) continue;
+  priority_queue<P, vector<P>, greater<P>>que;
+  que.push(make_pair(T(0), start));
+  dist[start] = T(0);
 
-      for (Edge ed: graph[front.second]) {
-        if (dist[ed.to] == T(-1) || dist[ed.to] > front.first + ed.cost) {
-          dist[ed.to] = front.first + ed.cost;
-          prev[ed.to] = front.second;
-          que.emplace(dist[ed.to], ed.to);
-        }
+  while (!que.empty()) {
+    P front = que.top(); que.pop();
+
+    if (dist[front.second] < front.first) continue;
+
+    for (Edge ed : graph[front.second]) {
+      if (dist[ed.to] == T(-1) || dist[ed.to] > front.first + ed.cost) {
+        dist[ed.to] = front.first + ed.cost;
+        prev[ed.to] = front.second;
+        que.emplace(dist[ed.to], ed.to);
       }
     }
-
-    return make_pair(dist, prev);  
   }
+
+  return make_pair(dist, prev);
 }
--- graph/graph_template.hpp
+++ graph/graph_template.hpp
@@ -1,38 +1,35 @@
 #pragma once
 #include <vector>
+template <typename T>
+struct Edge {
+  int from; int to;
+  T cost;
 
-namespace std {
-  template <typename T>
-  struct Edge {
-    int from; int to;
-    T cost;
+  Edge(int _from, int _to, T _cost) : from(_from), to(_to), cost(_cost) {}
 
-    Edge(int _from, int _to, T _cost) : from(_from), to(_to), cost(_cost) {}
+  // unweighted
+  Edge(int _from, int _to) : from(_from), to(_to), cost(T(1)) {}
 
-    // unweighted
-    Edge(int _from, int _to) : from(_from), to(_to), cost(T(1)) {}
-
-  };
+};
 
 
-  template <typename T>
-  struct Graph : vector<vector<Edge<T>>> {
+template <typename T>
+struct Graph : vector<vector<Edge<T>>> {
 
-    using vector<vector<Edge<T>>>::vector; // inherit constructors
+  using vector<vector<Edge<T>>>::vector; // inherit constructors
 
-    void add_edge(int i, Edge<T> e) {
-      (*this)[i].push_back(e);
-    }
+  void add_edge(int i, Edge<T> e) {
+    (*this)[i].push_back(e);
+  }
 
-    // weighted
-    void add_edge(int _from, int _to, T _cost) {
-      (*this)[_from].push_back(Edge(_from, _to, _cost));
-    }
+  // weighted
+  void add_edge(int _from, int _to, T _cost) {
+    (*this)[_from].push_back(Edge(_from, _to, _cost));
+  }
 
-    // unweighted
-    void add_edge(int _from, int _to) {
-      (*this)[_from].push_back(Edge(_from, _to, T(1)));
-    }
+  // unweighted
+  void add_edge(int _from, int _to) {
+    (*this)[_from].push_back(Edge(_from, _to, T(1)));
+  }
 
-  };
-}
+};
