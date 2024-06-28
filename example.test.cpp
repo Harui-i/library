16f5510a34c059a33bea733667fe76348fff4ba4
Haruki Oshiro <23120005@ed.tmu.ac.jp>
2024-06-28 18:30:38 +0900

    change test template

--- example.test.cpp
+++ example.test.cpp
@@ -1,23 +1,19 @@
 #define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/lesson/1/ALDS1/4/ALDS1_4_B"
-#include <algorithm>
-#include <iostream>
-#include <vector>
-#define REP(i, n) for (int i = 0; (i) < (int)(n); ++ (i))
-#define ALL(x) std::begin(x), std::end(x)
-using namespace std;
+#include "template/template.hpp"
 
 int main() {
-    int n; cin >> n;
-    vector<int> s(n);
-    REP (i, n) {
-        cin >> s[i];
-    }
-    int q; cin >> q;
-    int cnt = 0;
-    while (q --) {
-        int t_i; cin >> t_i;
-        cnt += binary_search(ALL(s), t_i);
-    }
-    cout << cnt << endl;
-    return 0;
+  ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+  int n; cin >> n;
+  vector<int> s(n);
+  for (int i = 0; i < n; i++) {
+    cin >> s[i];
+  }
+  int q; cin >> q;
+  int cnt = 0;
+  while (q--) {
+    int t_i; cin >> t_i;
+    cnt += binary_search(s.begin(), s.end(), t_i);
+  }
+  cout << cnt << endl;
+  return 0;
 }
