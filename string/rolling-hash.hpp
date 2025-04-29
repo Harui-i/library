dc6f15c17c37258590a78c33a6aa7ffd2657cda3
Haruki Oshiro <harukioshiro875421@gmail.com>
2025-04-29 18:43:20 +0900

    add utils

--- string/rolling-hash.hpp
+++ string/rolling-hash.hpp
@@ -3,6 +3,7 @@
 
 #include <iostream>
 #include <vector>
+#include <string>
 
 #include "math/modint2611.hpp"
 
@@ -12,7 +13,27 @@ struct Hash {
   mint base;
   int len;
 
+  // コンストラクタ
   constexpr Hash(mint _hash, mint _base, int _len): hash(_hash), base(_base), len(_len) {
+  }
+
+  // 文字列sのハッシュを返す
+  constexpr Hash(std::string s, mint _base): hash(mint(0)), base(_base) {
+    hash = mint(0);
+    base = _base;
+    len = int(s.size());
+
+    mint pow = mint(1);
+    for (int i=0; i<int(s.size()); i++){
+      hash += pow * mint(s[i]);
+      pow *= base;
+    }
+  }
+
+  // 文字cのハッシュを返す
+  constexpr Hash(char c, mint _base): base(_base) {
+    hash = mint(c) * base;
+    len = 1;
   }
 
   // 文字列としての結合をしたあとのHashを返す
@@ -23,12 +44,37 @@ struct Hash {
     return *this;
   }
 
+  // 文字列としての結合をしたあとのHashを返す
+  constexpr Hash& operator+=(const char& c) {
+    hash += base.pow(len) * mint(c);
+    len++;
+    return *this;
+  }
+
   constexpr Hash operator+(const Hash& rhs) const {
     return Hash(*this) += rhs; 
   }
 
+  constexpr Hash operator+(const char& c) const {
+    return Hash(*this) + c;
+  }
+
   constexpr bool operator==(const Hash& rhs) const {
     return len == rhs.len && hash == rhs.hash;
+  }
+
+  bool operator<(const Hash& rhs) const {
+    assert(base == rhs.base);
+    if (hash.val() < rhs.hash.val()) return true;
+    if (hash.val() > rhs.hash.val()) return false;
+    return len < rhs.len;
+  }
+
+  bool operator>(const Hash& rhs) const {
+    assert(base == rhs.base);
+    if (hash.val() > rhs.hash.val()) return false;
+    if (hash.val() < rhs.hash.val()) return true;
+    return len > rhs.len;
   }
 
   friend std::ostream& operator<<(std::ostream& os, const Hash& hash) {
