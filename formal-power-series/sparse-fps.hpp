a9f8dec2ee5348c15df4b5087c43c638b332d640
Haruki Oshiro <23120005@ed.tmu.ac.jp>
2025-07-02 14:01:53 +0900

    exp of sparse fps

--- formal-power-series/sparse-fps.hpp
+++ formal-power-series/sparse-fps.hpp
@@ -2,22 +2,33 @@
 #define HARUILIB_FORMAL_POWER_SERIES_SPARSE_FPS_HPP
 
 #include <vector>
-using namespace std;
 
 #include "formal-power-series/formal-power-series.hpp"
 
+
+// FPSの非ゼロな項を集めたvector<pair<int,mint>>を返す
+template <typename mint>
+std::vector<std::pair<int,mint>> get_nonzeros(const FPS<mint>& f) {
+  std::vector<std::pair<int,mint>> ret;
+  for (int i=0; i<f.size(); i++) {
+    if (f[i] != mint(0)) ret.emplace_back(i, f[i]);
+  }
+  return ret;
+}
+
+// ↓--- inverse of sparse fps ---↓
 // calculate inverse of f(sparse)
 // deg : -1 + ( maximum degree of g )
 template <typename mint>
-FPS<mint> inv_sparse(const vector<pair<int,mint>>& f, int deg) {
+FPS<mint> inv_sparse(const std::vector<std::pair<int,mint>>& f, int deg) {
   assert(deg >= 0);
   for(int i=0; i<(int)f.size()-1; i++) assert(f[i].first < f[i+1].first); 
   assert(f[0].first == 0 && f[0].second != mint(0));
 
   mint f0inv = f[0].second.inv();
-  vector<mint> g(deg); g[0] = f0inv;
+  std::vector<mint> g(deg); g[0] = f0inv;
   for(int i=0; i<deg-1; i++) {
-    for (pair<int,mint> pim : f) {
+    for (std::pair<int,mint> pim : f) {
       if (i+1 - pim.first >= 0) g[i+1] -= pim.second * g[i+1 - pim.first];
       else continue;
     }
@@ -29,20 +40,51 @@ FPS<mint> inv_sparse(const vector<pair<int,mint>>& f, int deg) {
 
 template <typename mint>
 FPS<mint> inv_sparse(const FPS<mint>& f, int deg) {
-  vector<pair<int,mint>> vpim;
-  for(int i=0; i<f.size(); i++) if (f[i] != mint(0)) vpim.emplace_back(i, f[i]);
+  return inv_sparse(get_nonzeros(f), deg);
+}
 
-  return inv_sparse(vpim, deg);
+// ↑--- inverse of sparse fps ----↑
+
+// exp(f)のdeg次未満の部分を求める。
+// F := exp(f) = F_0 + F_1 x + F_2 x^2 + ... とする。
+// F' = F * f' なので
+// F_1 + 2F_2 x + 3F_3 x^3 + ... = f' F.
+// 0以上の整数iについて、i次の項に注目すると、
+// (i+1) * F_{i+1} = [x^i] (f' * F)
+// とわかる。Fは0,1,...,i次までわかってればF_{i+1}もわかるということになる。f'はスパースだからF_{i+1}はたかだかK回の計算で求められる.
+
+template <typename mint>
+FPS<mint> exp_sparse(const FPS<mint>& f, int deg) {
+  FPS<mint> F(deg);
+  F[0] = mint(1);
+
+  std::vector<std::pair<int,mint>> nonzero_fdiff = get_nonzeros(f.diff());
+
+  for (int i=0; i+1<deg; i++) {
+    // F[i+1]を求める
+    // (i+1) * F_{i+1} = [x^i] (f' * F)
+
+    for (std::pair<int,mint> pim: nonzero_fdiff) {
+      int a = pim.first;
+      // Fのi-a次の項を足していく
+      if (i-a < 0) continue;
+      assert(i-a >= 0);
+      assert(i+1 > i-a);
+      F[i+1] += pim.second * F[i-a];
+    }
+    F[i+1] /= mint(i+1);
+  }
+  return F;
 }
 
 
-//tabun baggute masu. TODO
+//tabun baggute masu. 
 template<typename mint>
-FPS<mint> multiply_sparse(const FPS<mint>& f, const vector<pair<int,mint>>& g, int deg = -1) {
+FPS<mint> multiply_sparse(const FPS<mint>& f, const std::vector<std::pair<int,mint>>& g, int deg = -1) {
   if (deg == -1) deg = f.size() - 1 + g.back().first + 1;
 
   FPS<mint> ret(deg);
-  for (pair<int,mint> pim : g) {
+  for (std::pair<int,mint> pim : g) {
     assert(pim.second != 0);
     if (pim.second == 0) continue;
 
@@ -57,7 +99,7 @@ FPS<mint> multiply_sparse(const FPS<mint>& f, const vector<pair<int,mint>>& g, i
 
 template <typename mint>
 FPS<mint> multiply_sparse(const FPS<mint>& f, const FPS<mint>& g, int deg = -1) {
-  vector<pair<int,mint>> vpmi;
+  std::vector<std::pair<int,mint>> vpmi;
 
   for(int i=0; i<g.size(); i++) if (g[i] != mint(0)) vpmi.emplace_back(i, g[i]);
 
---
+++ test/verify/fps/yosupo-exp-of-formal-power-series-sparse.test.cpp
@@ -0,0 +1,23 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/exp_of_formal_power_series_sparse"
+
+#include "template/template.hpp"
+#include "math/modint.hpp"
+#include "formal-power-series/formal-power-series.hpp"
+#include "formal-power-series/fps998.hpp"
+#include "formal-power-series/sparse-fps.hpp"
+
+using mint = modint998244353;
+
+
+int main() {
+  ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+  int N, K; cin >> N >> K;
+  FPS<mint> f(N);
+  for (int i=0; i<K; i++) {
+    int a, x; cin >> a >> x;
+    f[a] = mint(x); 
+  }
+
+  cout << exp_sparse(f, N) << endl;
+
+}
