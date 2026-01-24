128f7ef22877be3a020aed912a3c5c3d6e4ff2e1
Haruki Oshiro <68176059+Harui-i@users.noreply.github.com>
2026-01-24 17:05:41 +0900

    オフライン二次元和ライブラリを実装

---
+++ structure/offline_2dsum.hpp
@@ -0,0 +1,83 @@
+#ifndef HARUILIB_STRUCTURE_OFFLINE_2DSUM_HPP
+#define HARUILIB_STRUCTURE_OFFLINE_2DSUM_HPP
+
+#include <vector>
+#include <algorithm>
+#include "structure/fenwick_tree.hpp"
+
+template <class T, class W> struct offline_2dsum {
+  struct Point {
+    T x, y;
+    W w;
+  };
+  struct Query {
+    T l, d, r, u;
+  };
+
+  void add_point(T x, T y, W w) {
+    points.push_back({x, y, w});
+  }
+
+  void add_query(T l, T d, T r, T u) {
+    queries.push_back({l, d, r, u});
+  }
+
+  std::vector<W> solve() {
+    int n = points.size();
+    int q = queries.size();
+    std::vector<W> res(q, 0);
+
+    std::vector<T> ys;
+    ys.reserve(n);
+    for (auto& p : points) ys.push_back(p.y);
+    std::sort(ys.begin(), ys.end());
+    ys.erase(std::unique(ys.begin(), ys.end()), ys.end());
+
+    auto get_y = [&](T y) {
+      return std::lower_bound(ys.begin(), ys.end(), y) - ys.begin();
+    };
+
+    struct Event {
+      T x;
+      int d, u;
+      int id;
+      int type; // -1 for l, 1 for r
+      bool operator<(const Event& other) const {
+        if (x != other.x) return x < other.x;
+        return type < other.type;
+      }
+    };
+
+    std::vector<Event> events;
+    events.reserve(2 * q);
+    for (int i = 0; i < q; i++) {
+      int d = get_y(queries[i].d);
+      int u = get_y(queries[i].u);
+      events.push_back({queries[i].l, d, u, i, -1});
+      events.push_back({queries[i].r, d, u, i, 1});
+    }
+
+    std::sort(events.begin(), events.end());
+    std::sort(points.begin(), points.end(), [](const Point& a, const Point& b) {
+      return a.x < b.x;
+    });
+
+    fenwick_tree<W> ft(ys.size());
+    int pi = 0;
+    for (auto& e : events) {
+      while (pi < n && points[pi].x < e.x) {
+        ft.add(get_y(points[pi].y), points[pi].w);
+        pi++;
+      }
+      res[e.id] += (W)e.type * ft.sum(e.d, e.u);
+    }
+
+    return res;
+  }
+
+private:
+  std::vector<Point> points;
+  std::vector<Query> queries;
+};
+
+#endif // HARUILIB_STRUCTURE_OFFLINE_2DSUM_HPP
